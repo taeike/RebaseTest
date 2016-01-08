320f0c0db06fa73b88688e65f8047adc597000f7
taeik e <tw2614@gmail.com>
2016-01-08 21:56:54 +0900

    mergeTest

--- app/src/main/java/com/example/taeike/rebasetest/MainActivity.java
+++ app/src/main/java/com/example/taeike/rebasetest/MainActivity.java
@@ -9,8 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  {
+//d
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
